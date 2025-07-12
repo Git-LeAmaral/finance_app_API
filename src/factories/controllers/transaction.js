d2d38a7b0bf2e3c5c04db4a63e909cf4735f1638
Leandro Amaral <lramaral02m@gmail.com>
2025-07-12 18:45:33 -0300

    feat: add CreateTransactionUseCase

---
+++ src/factories/controllers/transaction.js
@@ -0,0 +1,7 @@
+// import { PostgresCreateTransactionRepository } from '../../repositories/postgres/index.js'
+
+// export const makeCreateTransactionController = () => {
+//     const createTransactionRepository =
+//         new PostgresCreateTransactionRepository()
+
+// }
